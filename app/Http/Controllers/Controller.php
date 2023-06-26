5db3f3adee9a4d4cd941b4c3c2b80a42947b84f2
FunkyZombie <splinter.hertz@gmail.com>
2023-06-27 05:53:56 +0700

    База данных. Запросы. Миграции

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,37 +11,4 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    public function getNews(int $id = null)
-    {
-        
-        $news = [];
-        $faker = Factory::create();
-        
-
-        if ($id) 
-        {
-            return [
-                'id'            => $id,
-                'title'         => $faker->jobTitle(),
-                'author'        => $faker->userName(),
-                'status'        => 'DRAFT',
-                'description'   => $faker->text(),
-                'created_at'    => now('Europe/Moscow')
-            ];
-        }
-        
-        for ($i = 1; $i <= 10; $i++) {
-            $news[] = [
-                'id'            => $i,
-                'title'         => $faker->jobTitle(),
-                'author'        => $faker->userName(),
-                'status'        => 'DRAFT',
-                'description'   => $faker->text(),
-                'created_at'    => now('Europe/Moscow')
-            ];
-        }
-
-        sort($news);
-        return $news;
-    }
 }
--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -8,4 +8,31 @@
             </div>
         </div>
     </div>
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <tr>
+                <th>#ID</th>
+                <th>Category</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Status</th>
+                <th>Date</th>
+                <th>Actions</th>
+            </tr>
+            @foreach ($newsList as $item)
+            <tr>
+                <td>{{$item->id}}</th>
+                <td> - </th>
+                <td>{{$item->title}}</td>
+                <td>{{$item->author}}</td>
+                <td>{{$item->status}}</td>
+                <td>{{$item->created_at}}</td>
+                <td>
+                    <a href="#">Edit</a>&nbsp;
+                    <a href="#" style="color: red">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+        <table>
+    </div>
 @endsection
